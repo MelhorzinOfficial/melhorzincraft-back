f47153fa1629abb0c710341c1130ca4eb8ab6027
Carlos Peliciari <cpeliciari@outlook.com>
2025-04-12 17:24:26 -0300

    fix: Remove unused DockerImage field from GameTemplate struct

--- internal/core/gtemplate/gtemplate.go
+++ internal/core/gtemplate/gtemplate.go
@@ -1,16 +1,14 @@
 package gtemplate
 
 import (
-	"github.com/MelhorzinOfficial/melhorzincraft-back/internal/core/dimage"
-	"github.com/MelhorzinOfficial/melhorzincraft-back/internal/core/repository"
 	"time"
+
+	"github.com/MelhorzinOfficial/melhorzincraft-back/internal/core/repository"
 )
 
 //go:generate mockgen -destination=./mock/mock_gtemplate.go -package=mock github.com/MelhorzinOfficial/melhorzincraft-back/internal/core/gtemplate Repository
 
 type GameTemplate struct {
-	DockerImage *dimage.DockerImage
-
 	Id            int    `gorm:"primaryKey"`
 	Name          string `gorm:"column:name"`
 	DockerCompose []byte `gorm:"column:docker_compose"`
